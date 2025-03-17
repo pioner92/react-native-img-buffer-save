ccddca949c894609eec73bec9559e50e1b45cea8
pioner921227 <alex.shumihin921227@gmail.com>
2025-03-17 19:01:47 +0100

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,17 @@ import { NativeModules } from 'react-native'
 
 
 declare global {
-  var saveImageToGallery:  (buffer: ArrayBufferLike) => void
+  var saveImageToGallery:  (buffer: ArrayBufferLike) => void;
 }
 
 
-let __saveImageToGallery = global.saveImageToGallery
+let __saveImageToGallery = global.saveImageToGallery;
 
 if (!__saveImageToGallery) {
   if (NativeModules.ImgBufferSave?.install) {
-    NativeModules.ImgBufferSave.install() // Вызываем native install метод
-    __saveImageToGallery = global.saveImageToGallery // Сохраняем глобальную ссылку на объект SqlDb
-    console.log('✅ react-native-img-buffer-save initialized successfully')
+    NativeModules.ImgBufferSave.install(); // Вызываем native install метод
+    __saveImageToGallery = global.saveImageToGallery; // Сохраняем глобальную ссылку на объект SqlDb
+    console.log('✅ react-native-img-buffer-save initialized successfully');
   }
 }
 export const saveImageToGallery = (buff:ArrayBuffer | Uint8Array)=> {
@@ -23,5 +23,5 @@ export const saveImageToGallery = (buff:ArrayBuffer | Uint8Array)=> {
   } else {
     throw new Error('Invalid argument type; must be ArrayBuffer or Uint8Array');
   }
-}
+};
 
